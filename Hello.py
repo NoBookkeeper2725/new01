d16f95097a056fb31a82c97fc40c873506b9459b
NoBookkeeper2725 <129295530+NoBookkeeper2725@users.noreply.github.com>
2023-10-04 23:49:25 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,18 @@
+'''num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+print(num1+num2)'''
+
+'''num = int(input("Enter: "))
+if num % 2 == 0:
+    print('Even')
+else:
+    print('Odd')'''
+
+C = int(input("Enter Celsius: "))
+F = (9/5)*C + 32
+print("Converted to Fahrenheit: ",F)
+
+list1 = []
+list1 = int(input("Enter values: "))
+print(list1)
+
